ed4954aa3d0754875686d7993b2684107ff83d6b
Atanas Zlatev <atanas.zlatev.1998@gmail.com>
2025-11-20 22:40:43 +0200

    BE create task logic

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -1,9 +1,30 @@
 const {Router} = require('express');
+const { createTask } = require('../services/taskService');
 
 const taskController = Router();
 
 taskController.post('/tasks/add-task', async(req,res)=>{
-    // TO-DO
+    const {title,description,priorityLevel,type,finishBefore} = req.body;
+
+    const newTask = {
+        title,
+        description,
+        priorityLevel,
+        type,
+        finishBefore
+    }
+
+    try{
+        const response = await createTask(newTask);
+
+        if(response){
+            res.status(201).json(response);
+        }
+
+    }catch(err){
+        res.status(400).json({message:err.message})
+    }
+    
 })
 
 
--- src/services/taskService.js
+++ src/services/taskService.js
@@ -1,3 +1,20 @@
-export async function createTask() {
-    //TO-DO
+const {TaskModel} = require('../models/taskModel');
+
+async function createTask(task) {
+   
+    const newTask = new TaskModel({
+        title:task.title,
+        description:task.description,
+        priorityLevel:task.priorityLevel,
+        type:task.type,
+        finishBefore:task.finishBefore
+    })
+
+    await newTask.save();
+
+    return newTask
+}
+
+module.exports = {
+    createTask
 }
