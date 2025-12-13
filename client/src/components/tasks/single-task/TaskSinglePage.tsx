775a7b513d952a93e3fdc4c1959b8f7242f4292b
Atanas Zlatev <atanas.zlatev.1998@gmail.com>
2025-12-13 20:22:50 +0200

    FE edit btn is now not visible if task is completed

--- client/src/components/tasks/single-task/TaskSinglePage.tsx
+++ client/src/components/tasks/single-task/TaskSinglePage.tsx
@@ -3,6 +3,7 @@ import { useParams } from 'react-router'
 import './TaskSinglePage.style.css';
 import { Button } from '@/components/ui/button';
 import { useTasks } from '@/hooks/useTasks';
+import { Badge } from '@/components/ui/badge';
 
 const TaskSinglePage = () => {
     const {taskId} = useParams();
@@ -30,13 +31,13 @@ const TaskSinglePage = () => {
         <div className='task-options m-3 rounded-lg justify-start flex flex-col bg-gray-200'>
             <h2 className='p-3 text-center font-bold'>Options</h2>
             <div className='ps-3'>
-                <p><span className='font-bold'>Completed: </span>{singleTask?.isCompleted === true ? 'True' : 'False'}</p>
+                <p><span className='font-bold'>Completed: </span><Badge variant={'secondary'} className={`text-white ${singleTask?.isCompleted ? 'bg-green-700':'bg-red-700'}`}>{singleTask?.isCompleted === true ? 'True' : 'False'}</Badge></p>
                 <p><span className='font-bold' >Finish Before:</span> {singleTask?.finishBefore.split('T')[0]}</p>
                 <p><span className='font-bold'>Type:</span> {singleTask?.type}</p>
                 <p><span className='font-bold'>Priority:</span> {singleTask?.priorityLevel}</p>
             </div>
              <div className='flex gap-2 p-3 justify-center'>
-                <Button variant='secondary' className='bg-green-500 cursor-pointer'>Edit</Button>
+                {singleTask?.isCompleted ? (null) : (<Button variant='secondary' className='bg-green-500 cursor-pointer'>Edit</Button>)}
                 <Button variant='destructive' className='cursor-pointer bg-red-500'>Remove</Button>
             </div>
         </div>
